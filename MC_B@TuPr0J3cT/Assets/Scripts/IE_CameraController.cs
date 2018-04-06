dcaa3e3b85285070e4b4f493c99f5f8f13ffae1a
Methien <batuerci@gmail.com>
2018-04-06 13:13:47 +0300

    Game Dev inc

---
+++ MC_B@TuPr0J3cT/Assets/Scripts/IE_CameraController.cs
@@ -0,0 +1,49 @@
+﻿
+using UnityEngine;
+
+public class IE_CameraController : MonoBehaviour
+{
+
+
+    private float f_InputX;
+    private float f_InputY;
+
+    private float f_SmoothedX;
+    private float f_SmoothedY;
+
+    private float f_RefX;
+    private float f_RefY;
+
+
+    public Vector2 v2_Sensitivity;
+    public float f_SmoothTime;
+
+	private Quaternion q_This;
+	private Quaternion q_Player;
+	public Transform t_Player;
+
+	void Awake()
+	{
+		q_This = transform.localRotation;
+		q_Player = t_Player.localRotation;
+	}
+    void Update()
+    {
+
+        f_InputX += Input.GetAxis("Mouse X") * v2_Sensitivity.x;
+        f_InputY -= Input.GetAxis("Mouse Y") * v2_Sensitivity.y;
+
+		// f_InputY'i Limitle. "Clamp"
+
+        f_SmoothedX = Mathf.SmoothDamp(f_SmoothedX, f_InputX, ref f_RefX, f_SmoothTime);
+        f_SmoothedY = Mathf.SmoothDamp(f_SmoothedY, f_InputY, ref f_RefY, f_SmoothTime);
+
+        Quaternion q_X = Quaternion.AngleAxis(f_SmoothedX, Vector3.up);
+        Quaternion q_Y = Quaternion.AngleAxis(f_SmoothedY, Vector3.right);
+
+		t_Player.localRotation = q_Player * q_X;
+        transform.localRotation = q_This * q_Y;
+    }
+
+
+}
---
+++ MC_B@TuPr0J3cT/Assets/Scripts/IE_PlayerController.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class IE_PlayerController : MonoBehaviour {
+
+	public float f_Speed;
+	public float f_JumpSpeed;
+	public float f_Gravity;
+	private Vector3 v3_MoveDirection = Vector3.zero;
+	public CharacterController cc_This;
+
+	void Update()
+	{
+		
+		// Input'u v3_MoveDirection'a sakla. "Horizontal" ve "Vertical"
+		// Scripting Reference CharacterController Move
+
+
+		cc_This.Move(v3_MoveDirection * Time.deltaTime);
+
+	}
+
+}
